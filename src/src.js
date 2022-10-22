b80bea5f84f5d90cfe26ae8b1c73fb39cd7ab095
João Torres <joao_torres@id.uff.br>
2022-10-22 19:13:52 -0300

    aula 7

---
+++ src/src.js
@@ -0,0 +1,64 @@
+function onChangeEmail() {
+  toggleButtonsDisable();
+  toggleEmailErrors();
+}
+
+function onChangePassword() {
+  toggleButtonsDisable();
+  togglePasswordErrors();
+}
+
+function isEmailValid() {
+  const email = form.email().value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
+}
+
+function isPasswordValid() {
+  const password = form.password().value;
+  if (!password) {
+    return false;
+  }
+  return true;
+}
+
+function toggleEmailErrors() {
+  const email = form.email().value;
+  form.emailRequiredError().style.display = email ? "none" : "block";
+  form.emailInvalidError().style.display = validateEmail(email)
+    ? "none"
+    : "block";
+}
+
+function togglePasswordErrors() {
+  const password = form.password().value;
+  form.passwordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toggleButtonsDisable() {
+  const emailValid = isEmailValid();
+  form.passwordRecover().disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  form.loginButton().disabled = !emailValid || !passwordValid;
+}
+
+const form = {
+  email: () => document.getElementById("email"),
+  emailInvalidError: () => document.getElementById("email-invalid-error"),
+  emailRequiredError: () => document.getElementById("email-required-error"),
+  loginButton: () => document.getElementById("login-button"),
+  password: () => document.getElementById("password"),
+  passwordRequiredError: () =>
+    document.getElementById("password-require-error"),
+  passwordRecover: () => document.getElementById("recover-password-button"),
+};
+
+function login() {
+  window.location.href = "views/home.html";
+}
+function register() {
+  window.location.href = "views/register.html";
+}
